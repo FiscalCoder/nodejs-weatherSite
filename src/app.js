a00c04e35f7e75d1fa834c0c6fc0dcb7bb2401b7
bharghav <bharghav@pop-os.localdomain>
2020-02-23 15:35:30 +0530

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const forecast = require('./utils/forecast')
 
 
 const app = express()
+const port = process.env.PORT || 3000
 
 //Define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -127,7 +128,8 @@ app.get('*', (req, res) =>
     })
 })
 
-app.listen(3000, () => 
+app.listen(port, () => 
 {
-    console.log('Server running...')
+    console.log('Server running on port...' + port)
 }) 
+
